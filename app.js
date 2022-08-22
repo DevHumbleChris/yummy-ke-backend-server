561750dd4fff9fb94d114ae4ec792b9614d50912
DevHumbleChris <dev.humblechris254@gmail.com>
2022-08-22 21:20:19 +0300

    Successfully Did setup ApolloServer

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+const { ApolloServer } = require('apollo-server-express')
+const { ApolloServerPluginDrainHttpServer, gql } = require('apollo-server-core')
+
+const typeDefs = gql`
+    type Friend {
+        name: String
+    }
+    type Query {
+        friends: [Friend]
+    }
+`
+const Friends = [
+    { name: 'Jacob' },
+    { name: 'Christopher'}
+]
+
+const resolvers = {
+    Query : {
+        friends: () => Friends
+    }
+}
+
+async function startApolloServer(typeDefs, resolvers) {
+    const app = express()
+    const httpServer = http.createServer(app)
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        cache: `bounded`,
+        csrfPrevention: true,
+        plugins: [
+            ApolloServerPluginDrainHttpServer({
+                httpServer
+            })
+        ]
+    })
+    await server.start()
+    server.applyMiddleware({
+        app,
+        path: '/graphql'
+    })
+    await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve ))
+    console.log(`GraphQL Server Up And Running at http://localhost:4000${server.graphqlPath}`)
+}
+
+startApolloServer(typeDefs, resolvers)
