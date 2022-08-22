64d691157ab7a22a0dfd40a0f7d20d7bf7ed06f2
DevHumbleChris <dev.humblechris254@gmail.com>
2022-08-22 21:36:11 +0300

    Added The Restaurant TypeDef and Resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -2,10 +2,21 @@ const Friends = [
     { name: 'Jacob' },
     { name: 'Christopher'}
 ]
-
+const restaurants = [
+    {
+        id: 2344,
+        name: 'Love',
+        category: 'Dining Out',
+        averageCost: 23.4,
+        cuisines: ['Biryani', 'Dessert'],
+        populaDishes: ['Pilau', 'Uji'],
+        facilities: ['WIFI', 'Bed And Breakfast']
+    }
+]
 const resolvers = {
     Query : {
-        friends: () => Friends
+        friends: () => Friends,
+        restaurants: () => restaurants
     }
 }
 
--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -1,11 +1,24 @@
 const { gql } = require("apollo-server-core");
 
 const typeDefs = gql`
+    scalar Date
+
     type Friend {
         name: String
     }
     type Query {
         friends: [Friend]
+        restaurants: [Restaurant]
+    }
+    type Restaurant {
+        id: ID
+        name: String!
+        category: String!
+        averageCost: Float!
+        cuisines: [String]
+        populaDishes: [String]
+        facilities: [String]
+        dateCreated: Date
     }
 `
 
