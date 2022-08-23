dd4afd707381e3e0246ae441ab90e5594bd70c56
DevHumbleChris <dev.humblechris254@gmail.com>
2022-08-23 21:17:39 +0300

    Added REST API

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const http = require('http')
 const cors = require('cors')
 const { ApolloServer } = require('apollo-server-express')
 const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
+const { router } = require('./routes')
 const typeDefs = require('./schema/typeDefs')
 const resolvers = require('./schema/resolvers')
 
+const app = express()
+const httpServer = http.createServer(app)
+const GRAPHQL_PORT = process.env.GRAPHQL_PORT
+
+// Middlewares.
+app.use(cors())
+app.use('/rest-api', router)
+
 async function startApolloServer(typeDefs, resolvers) {
-    const app = express()
-    const httpServer = http.createServer(app)
     const server = new ApolloServer({
         typeDefs,
         resolvers,
@@ -25,8 +33,11 @@ async function startApolloServer(typeDefs, resolvers) {
         app,
         path: '/graphql'
     })
-    await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve ))
-    console.log(`GraphQL Server Up And Running at http://localhost:4000${server.graphqlPath}`)
+    await new Promise(resolve => httpServer.listen({ port: GRAPHQL_PORT }, resolve ))
+    console.log(`
+    🚀 GraphQL Server Up And Running at http://localhost:4000${server.graphqlPath}
+    😎 REST API Server Up And Running at http://localhost:4000/rest-api
+    `)
 }
 
 startApolloServer(typeDefs, resolvers)
